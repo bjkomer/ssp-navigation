54ef9ffc246e539af4f69aa85ed40dd5a582fef5
Brent Komer <brent.komer@gmail.com>
2019-11-25 17:45:58 -0500

    fixing typo

--- ssp_navigation/generate_datasets.py
+++ ssp_navigation/generate_datasets.py
@@ -16,7 +16,7 @@ parser = argparse.ArgumentParser(
 
 # General maze and SSP parameters
 parser.add_argument('--maze-size', type=int, default=13, help='Size of the coarse maze structure')
-parser.add_argument('--map-style', type=str, default='maze', choices=['blocks', 'maze', 'mixed', 'corridor'], help='Style of maze')
+parser.add_argument('--map-style', type=str, default='maze', choices=['blocks', 'maze', 'mixed', 'corridors'], help='Style of maze')
 parser.add_argument('--res', type=int, default=64, help='resolution of the fine maze')
 # parser.add_argument('--limit-low', type=float, default=0, help='lowest coordinate value')
 # parser.add_argument('--limit-high', type=float, default=13, help='highest coordinate value')
