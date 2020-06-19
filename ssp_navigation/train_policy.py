60857eb5ebf6236e6e398c28599d4d68dd9f5282
Brent Komer <brent.komer@gmail.com>
2020-06-19 16:15:17 -0400

    wrapping dataframe generation into the training script

--- ssp_navigation/train_policy.py
+++ ssp_navigation/train_policy.py
@@ -15,6 +15,8 @@ from ssp_navigation.utils.models import FeedForward, MLP, LearnedEncoding, Learn
 from ssp_navigation.utils.encodings import get_encoding_function
 # from functools import partial
 import nengo.spa as spa
+from ssp_navigation.utils.training import PolicyEvaluation
+import pandas as pd
 
 parser = argparse.ArgumentParser(
     'Train a function that given a maze and a goal location, computes the direction to move to get to that goal'
@@ -79,7 +81,7 @@ parser.add_argument('--momentum', type=float, default=0.9)
 parser.add_argument('--weight-decay', type=float, default=0, help='strength of L2 weight normalization')
 parser.add_argument('--weight-histogram', action='store_true', help='Save histogram of the weights')
 parser.add_argument('--res', type=int, default=64, help='resolution of the fine maze')
-parser.add_argument('--dataset-dir', type=str, default='datasets/mixed_style_20mazes_50goals_64res_13size_13seed')
+parser.add_argument('--dataset-dir', type=str, default='datasets/mixed_style_100mazes_100goals_64res_13size_13seed')
 parser.add_argument('--no-wall-overlay', action='store_true',
                     help='Do not use rainbow colours and wall overlay in validation images')
 parser.add_argument('--optimizer', type=str, default='rmsprop', choices=['rmsprop', 'adam', 'sgd'])
@@ -96,6 +98,8 @@ parser.add_argument('--shift-noise', type=float, default=0.0,
 
 parser.add_argument('--gpu', type=int, default=-1,
                     help="set to an integer corresponding to the gpu to use. Set to -1 to use the CPU")
+
+parser.add_argument('--out-file', type=str, default="", help='Output file name')
 
 args = parser.parse_args()
 
@@ -118,23 +122,23 @@ if os.path.exists(logdir):
     print(logdir)
     sys.exit(0)
 
-data = np.load(dataset_file)
+dataset = np.load(dataset_file)
 
 rng = np.random.RandomState(seed=args.seed)
 torch.manual_seed(args.seed)
 np.random.seed(args.seed)
 
 # n_mazes by res by res
-fine_mazes = data['fine_mazes']
+fine_mazes = dataset['fine_mazes']
 
 # # n_mazes by res by res by 2
-# solved_mazes = data['solved_mazes']
+# solved_mazes = dataset['solved_mazes']
 
 # # n_mazes by dim
-# maze_sps = data['maze_sps']
+# maze_sps = dataset['maze_sps']
 
 # n_mazes by n_goals by 2
-goals = data['goals']
+goals = dataset['goals']
 
 n_goals = goals.shape[1]
 n_mazes = fine_mazes.shape[0]
@@ -148,7 +152,7 @@ else:
 
 if args.maze_id_type == 'ssp':
     id_size = args.maze_id_dim
-    maze_sps = data['maze_sps']
+    maze_sps = dataset['maze_sps']
 elif args.maze_id_type == 'one-hot':
     id_size = n_mazes
     # overwrite data
@@ -169,7 +173,7 @@ limit_low = 0
 if args.encoding_limit != 0.0:
     limit_high = args.encoding_limit
 else:
-    limit_high = data['coarse_mazes'].shape[2]
+    limit_high = dataset['coarse_mazes'].shape[2]
 
     # modify the encoding limit to account for all of the environments
     if args.tile_mazes:
@@ -178,7 +182,7 @@ else:
 encoding_func, repr_dim = get_encoding_function(args, limit_low=limit_low, limit_high=limit_high)
 
 # Create a validation/visualization set to run periodically while training and at the end
-# validation_set = ValidationSet(data=data, maze_indices=np.arange(n_mazes), goal_indices=[0])
+# validation_set = ValidationSet(data=dataset, maze_indices=np.arange(n_mazes), goal_indices=[0])
 
 # Set up number of mazes/goals to view in the viz set based on how many are available
 if n_mazes < 4:
@@ -195,7 +199,7 @@ else:
         goal_indices = [0, 1]
 
 validation_set = PolicyValidationSet(
-    data=data, dim=repr_dim, maze_sps=maze_sps, maze_indices=maze_indices, goal_indices=goal_indices, subsample=args.subsample,
+    data=dataset, dim=repr_dim, maze_sps=maze_sps, maze_indices=maze_indices, goal_indices=goal_indices, subsample=args.subsample,
     # spatial_encoding=args.spatial_encoding,
     tile_mazes=args.tile_mazes,
     connected_tiles=args.connected_tiles,  # NOTE: viz set currently does not use this
@@ -203,7 +207,7 @@ validation_set = PolicyValidationSet(
 )
 
 trainloader, testloader = create_train_test_dataloaders(
-    data=data, n_train_samples=args.n_train_samples, n_test_samples=args.n_test_samples,
+    data=dataset, n_train_samples=args.n_train_samples, n_test_samples=args.n_test_samples,
     input_noise=args.input_noise,
     shift_noise=args.shift_noise,
     maze_sps=maze_sps, args=args, n_mazes=args.n_mazes,
@@ -435,3 +439,104 @@ if args.logdir != '':
 
     with open(os.path.join(save_dir, "params.json"), "w") as f:
         json.dump(params, f)
+
+print("Running evaluation script")
+if args.out_file == '':
+    print("No eval file given, skipping dataframe generation")
+else:
+    print("Generating data for:")
+    print(args.out_file)
+
+    validation_set = PolicyEvaluation(
+        data=dataset, dim=repr_dim, maze_sps=maze_sps,
+        encoding_func=encoding_func, device=device,
+        n_train_samples=args.n_train_samples,
+        n_test_samples=args.n_test_samples,
+        spatial_encoding=args.spatial_encoding,
+        n_mazes=n_mazes,
+        tile_mazes=args.tile_mazes,
+        connected_tiles=args.connected_tiles,
+    )
+
+    # Reset seeds here after generating data
+    torch.manual_seed(args.seed)
+    np.random.seed(args.seed)
+
+    # avg_rmse_train, avg_angle_rmse_train, avg_rmse_test, avg_angle_rmse_test = validation_set.get_rmse(model)
+    # train_r2, test_r2 = validation_set.get_r2_score(model)
+
+    avg_rmse_train, avg_angle_rmse_train, avg_rmse_test, avg_angle_rmse_test, train_r2, test_r2, avg_mse_loss_train, avg_mse_loss_test = validation_set.get_r2_and_rmse(model)
+
+    # Keep track of global and local RMSE separately for connected tiles
+    if args.connected_tiles:
+        global_avg_rmse_train, global_avg_angle_rmse_train, global_avg_rmse_test, global_avg_angle_rmse_test = validation_set.get_global_rmse(model)
+        df = pd.DataFrame(
+            data=[[(avg_rmse_train + global_avg_rmse_train)/2.,
+                   (avg_angle_rmse_train + global_avg_angle_rmse_train)/2.,
+                   (avg_rmse_test + global_avg_rmse_test)/2.,
+                   (avg_angle_rmse_test + global_avg_angle_rmse_test)/2.,
+                   avg_rmse_train, avg_angle_rmse_train, avg_rmse_test, avg_angle_rmse_test,
+                   global_avg_rmse_train, global_avg_angle_rmse_train, global_avg_rmse_test, global_avg_angle_rmse_test,
+                   train_r2, test_r2,
+                   ]],
+            columns=['Train RMSE', 'Train Angular RMSE', 'RMSE', 'Angular RMSE',
+                     'Local Train RMSE', 'Local Train Angular RMSE', 'Local RMSE', 'Local Angular RMSE',
+                     'Global Train RMSE', 'Global Train Angular RMSE', 'Global RMSE', 'Global Angular RMSE',
+                     'Train R2', 'R2'
+                     ],
+        )
+    else:
+        df = pd.DataFrame(
+            data=[[avg_rmse_train, avg_angle_rmse_train, avg_rmse_test, avg_angle_rmse_test, train_r2, test_r2, avg_mse_loss_train, avg_mse_loss_test]],
+            columns=['Train RMSE', 'Train Angular RMSE', 'RMSE', 'Angular RMSE', 'Train R2', 'R2', 'Train Loss', 'Test Loss'],
+        )
+
+    df['Dimensionality'] = args.dim
+    df['Hidden Layer Size'] = args.hidden_size
+    df['Hidden Layers'] = args.n_hidden_layers
+    df['Encoding'] = args.spatial_encoding
+    df['Seed'] = args.seed
+    df['Maze ID Type'] = args.maze_id_type
+    df['Maze ID Dim'] = args.maze_id_dim
+    df['Tile Mazes'] = args.tile_mazes
+    df['Dropout Fraction'] = args.dropout_fraction
+    df['SSP Scaling'] = args.ssp_scaling
+
+    if args.spatial_encoding == 'sub-toroid-ssp':
+        df['Proj Dim'] = args.n_proj
+        df['Scale Ratio'] = args.scale_ratio
+
+    if args.spatial_encoding == 'proj-ssp':
+        df['Proj Dim'] = args.n_proj
+
+    if args.spatial_encoding == 'grid-ssp':
+        df['Grid SSP Min'] = args.grid_ssp_min
+        df['Grid SSP Max'] = args.grid_ssp_max
+
+    if (args.spatial_encoding == 'pc-gauss') or (args.spatial_encoding == 'pc-dog'):
+        df['Hilbert Curve'] = args.hilbert_points
+        df['Sigma'] = args.pc_gauss_sigma
+        if args.spatial_encoding == 'pc-dog':
+            df['Diff Sigma'] = args.pc_diff_sigma
+
+    if (args.spatial_encoding == 'random-trig') or (args.spatial_encoding == 'random-rotated-trig'):
+        df['Freq Limit'] = args.freq_limit
+
+    # # Other differentiators
+    # df['Number of Mazes Tested'] = args.n_mazes_tested
+    # df['Number of Goals Tested'] = args.n_goals_tested
+
+    # Command line supplied tags
+    # df['Dataset'] = args.dataset  # mixed
+    # df['Trained On'] = args.trained_on
+    df['Epochs'] = args.epochs
+    df['Batch Size'] = args.batch_size
+    df['Number of Mazes'] = args.n_mazes
+    df['Loss Function'] = args.loss_function
+    df['Learning Rate'] = args.lr
+    df['Momentum'] = args.momentum
+    df['Optimizer'] = args.optimizer
+    df['Input Noise'] = args.input_noise
+    df['Shift Noise'] = args.shift_noise
+
+    df.to_csv(args.out_file)
